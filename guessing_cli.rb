297882d32f13970fd0ef1f5077b7b52a5040243e
Zachary <zbberman@yahoo.com>
2019-02-07 22:31:30 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
-  user_input = gets.chomp
   computer_number = rand(1..6)
   while user_input != computer_number
     puts "Guess a number between 1 and 6."
+    user_input = gets.chomp
   end 
 end 
